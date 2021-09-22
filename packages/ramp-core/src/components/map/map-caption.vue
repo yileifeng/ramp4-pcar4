7d17a7ce49b5200e9e87f6ef7dc36cfa65720c67
Yi Lei Feng <fengyilei98@hotmail.com>
2021-09-22 10:59:11 -0400

    fix mouse coords not showing up in web mercator format

--- packages/ramp-core/src/components/map/map-caption.vue
+++ packages/ramp-core/src/components/map/map-caption.vue
@@ -101,7 +101,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import { get } from '@/store/pathify-helper';
 import { Attribution, ScaleBarProperties } from '@/geo/api';
 import { GlobalEvents } from '@/api';
